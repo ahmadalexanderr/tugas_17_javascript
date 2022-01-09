c134f02c792a89590820d640c4c1d807562ade0e
Ahmad Alexander <ahmadalexanderr@gmail.com>
2022-01-09 19:23:14 +0700

    Upload Tugas 17 JS

---
+++ index.js
@@ -0,0 +1,10 @@
+var string1 = "Saya beLajar bahaSa peMrograman deNgan khUsuk."
+var string2 = "Allen SedAng MemAncinG IkaN di SuNgai, DIA MEndapat IkAN GaBuS 3 Ekor dan LanGsuNg Dijual."
+
+const lowUpOp = (x) => {
+    console.log(x.toLowerCase())
+    console.log(x.toUpperCase())
+}
+
+lowUpOp(string1)
+lowUpOp(string2)
